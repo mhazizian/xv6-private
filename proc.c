f3e99523003e0e5ceb1875d494d7594c00d932c3
Mohammad Hosein Azizian <mohammadh.azizian@gmail.com>
2018-11-11 11:03:52 +0330

    Add number of system call to control-p in xv6

--- proc.c
+++ proc.c
@@ -591,11 +591,12 @@ procdump(void)
 			state = states[p->state];
 		else
 			state = "???";
-		cprintf("%d %s %s", p->pid, state, p->name);
+		cprintf("%d %s %s count: %d", p->pid, state, p->name, process_system_calls[p->pid].number_of_calls);
 		if(p->state == SLEEPING){
 			getcallerpcs((uint*)p->context->ebp+2, pc);
 			for(i=0; i<10 && pc[i] != 0; i++)
 				cprintf(" %p", pc[i]);
+
 		}
 		cprintf("\n");
 	}
