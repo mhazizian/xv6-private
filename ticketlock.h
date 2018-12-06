4badbb96b49c0fc23a0d88666cb894e24f00e5c7
Mohammad Hosein Azizian <mohammadh.azizian@gmail.com>
2018-12-06 10:20:21 +0330

    remove myspinlock struct

--- ticketlock.h
+++ ticketlock.h
@@ -1,18 +1,18 @@
 // Mutual exclusion lock.
-struct myspinlock {
-	uint locked;			 // Is the lock held?
+// struct myspinlock {
+// 	uint locked;			 // Is the lock held?
 
-	// For debugging:
-	char *name;				// Name of lock.
-	struct cpu *cpu;	 // The cpu holding the lock.
-	uint pcs[10];			// The call stack (an array of program counters)
-	                                                                         // that locked the lock.
-};
+// 	// For debugging:
+// 	char *name;				// Name of lock.
+// 	struct cpu *cpu;	 // The cpu holding the lock.
+// 	uint pcs[10];			// The call stack (an array of program counters)
+// 	                                                                         // that locked the lock.
+// };
 
 // Ticketlock for processes
 struct ticketlock {
 	uint locked;			 // Is the lock held?
-	struct myspinlock lk; // spinlock protecting this ticket lock
+	// struct myspinlock lk; // spinlock protecting this ticket lock
 	int pid;					 // Process holding lock
 	int next_ticket;
 	int now_serving_ticket;
