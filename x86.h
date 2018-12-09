b6afa149e685378b4c30a7321aabb9b38eb71f39
Kamran <kamran2727@gmail.com>
2018-12-09 20:56:03 +0330

    code refactoring(Remove extra comment)

--- x86.h
+++ x86.h
@@ -130,15 +130,6 @@ xchg(volatile uint *addr, uint newval)
 	return result;
 }
 
-// static inline int fetch_and_add(int* variable, int value)
-// {
-// 	__asm__ volatile("lock; xaddl %0, %1"
-// 	: "+r" (value), "+m" (*variable) // input+output
-// 	: // No input-only
-// 	: "memory"
-// 	);
-// 	return value;
-// }
 static inline unsigned int
 fetch_and_add(volatile unsigned int* p, unsigned int incr)
 {
