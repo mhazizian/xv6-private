2b887c742ce450e1af7a54d7c39dc9e69ba4a406
Mohammad Hosein Azizian <mohammadh.azizian@gmail.com>
2018-12-10 08:17:42 +0330

    Add headergard

--- ticketlock.h
+++ ticketlock.h
@@ -1,6 +1,11 @@
+#ifndef _TICKET_LOCK_H_
+#define _TICKET_LOCK_H_
+
 // Ticketlock for processes
 struct ticketlock {
 	unsigned int pid;					 // Process holding lock
 	unsigned int next_ticket;
 	unsigned int now_serving_ticket;
 };
+
+#endif
