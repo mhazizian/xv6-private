cecfde176908124558f8215cd50fb51970ac2a94
Kamran <kamran2727@gmail.com>
2019-01-14 19:02:10 +0330

    Amend up

--- up.c
+++ up.c
@@ -6,24 +6,25 @@
 
 int main(int argc, char *argv[])
 {
-    shm_open(1, 10, 0);
+    char* va = shm_open(1, 1, 0);
 
     if (fork() == 0)
     {
-        char* virt_addr = shm_open(2, 10, 0);
-        printf(1, "***********this is return of shm_open: %d\n ", shm_open(3, 10, 0));
-        virt_addr = shm_open(4, 10, 0);
+        shm_open(3, 10, 0);
+        char* virt_addr = shm_open(4, 10, 0);
         *virt_addr = 'c';
-        printf(1, "write done  %p\n", virt_addr);
+//        printf(1, "------write done  %p\n", virt_addr);
+        *virt_addr = 'g';
+        printf(1, "child: write done\n");
         exit();
-    }
-    else
-        {
-            sleep(100);
-            printf(1, "read character\n");
-            char * virt_addr = shm_attach(2);
-            printf(1, "virtual address in parent: %p\n", virt_addr);
-            printf(1, "read character in parent: %c\n", *virt_addr);
+    } else
+    {
+        sleep(100);
+//        printf(1, "read character\n");
+        char * virt_addr = shm_attach(4);
+//        printf(1, "virtual address in parent: %p\n", virt_addr);
+        printf(1, "read character in parent------: --%c--\n", *virt_addr);
+//        printf(1, "read character in parent va-----------: --%c--\n", *va);
     }
 
 
